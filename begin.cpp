13ee53540f18174d8e6aa528053e6d8ff621d6dd
SunHongJae <sunhongjae@gmail.com>
2025-07-04 20:36:08 +0900

    new changes

---
+++ begin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Hello Wordl!"<<endl;
+    cout<<"어라 왜 또 이러는거야!!"<<endl;
+    cin>>num;
+    cout<<num+100<<endl;
+    return 0;
+}
