3a7db397a810038b8939e0dcf1f4acdaabb34ec9
SunHongJae <sunhongjae@gmail.com>
2025-07-04 22:52:06 +0900

    make numSum

--- begin.cpp
+++ begin.cpp
@@ -8,7 +8,13 @@ int main(){
     cin>>num;
     cout<<num+100<<endl;
     cout<<"안녕하세요 git을 이용해서 새 변경점을 만들려구요.."<<endl;
-    cout<<"2번째 줄은 연습삼아 삭제해봤어요"<<endl;
     cout << "num의 곱연산: " + num * 200 << endl;
+    cout << "num과 50의 합은: " + numSum(num) << endl;
     return 0;
+}
+
+int numSum(int num){
+    num += 50;
+    cout<< num << endl;
+    return num;
 }
