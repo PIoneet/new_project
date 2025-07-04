c0b4bb04db55d70b34244c419a7fe5a8063f245f
SunHongJae <sunhongjae@gmail.com>
2025-07-04 20:42:55 +0900

    delete features

--- begin.cpp
+++ begin.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
     int num;
     cout<<"Hello Wordl!"<<endl;
-    cout<<"어라 왜 또 이러는거야!!"<<endl;
     cin>>num;
     cout<<num+100<<endl;
+    cout<<"안녕하세요 git을 이용해서 새 변경점을 만들려구요.."<<endl;
+    cout<<"2번째 줄은 연습삼아 삭제해봤어요"<<endl;
     return 0;
 }
