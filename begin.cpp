a6eef15ddad9a6996e49eafb5e909c2c13faa590
SunHongJae <sunhongjae@gmail.com>
2025-07-04 22:58:14 +0900

    numSum append

--- begin.cpp
+++ begin.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 using namespace std;
+int numSum(int num);
 
 int main(){
     int num;
     cout<<"Hello Wordl!"<<endl;
     cin>>num;
     cout<<num+100<<endl;
-    cout<<"안녕하세요 git을 이용해서 새 변경점을 만들려구요.."<<endl;
     cout << "num의 곱연산: " + num * 200 << endl;
     cout << "num과 50의 합은: " + numSum(num) << endl;
     return 0;
